4273add7383698699c55f1f4561b52d516c5505c
Jason Staten <jstaten07@gmail.com>
2010-01-21 18:54:11 -0700

    Added in default.aspx and changed project type guid to not require MVC2 to open

---
+++ WindsorMefMvc/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace WindsorMefMvc.Web
+{
+	public class Default : Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			string originalPath = Request.Path;
+			HttpContext.Current.RewritePath(Request.ApplicationPath, false);
+			IHttpHandler httpHandler = new MvcHttpHandler();
+			httpHandler.ProcessRequest(HttpContext.Current);
+			HttpContext.Current.RewritePath(originalPath, false);
+		}
+	}
+}
